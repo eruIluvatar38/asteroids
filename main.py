52f1b1d94e5e24db964bdfa8c1b73a661a664177
eruiluvatar38 <krmrsriharsha@gmail.com>
2025-02-12 20:14:40 -0800

    update player

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main() :
     pygame.init()
@@ -11,13 +13,31 @@ def main() :
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clk = pygame.time.Clock()
+    dt = 0
+
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+            
         screen.fill((0, 0, 0))
+
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+    
         pygame.display.flip()
+        dt = clk.tick(60)/1000
 
 if __name__ == "__main__":
     main()
